3dca56555de1c52e6e4e1bf5d60eb12998255236
AlbertoDeTeresaChaves <alberto.trabajo.es@gmail.com>
2024-02-13 09:01:00 +0100

    Agregando parte 1 del menu

--- app/src/main/java/com/example/trabajo_final/menu_opciones.java
+++ app/src/main/java/com/example/trabajo_final/menu_opciones.java
@@ -2,7 +2,13 @@ package com.example.trabajo_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class menu_opciones extends AppCompatActivity {
 
@@ -11,4 +17,11 @@ public class menu_opciones extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_opciones);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
